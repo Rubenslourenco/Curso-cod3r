367d7094f58bcd1dcab76e16e26e854bd98e4932
Rubens Alves <alvesrubens926@gmail.com>
2024-10-28 23:07:59 -0300

    Atualizando as aulas de objeto com o repositorio remoto

--- objeto/heranca02.js
+++ objeto/heranca02.js
@@ -1,6 +1,13 @@
+//Cadeia de prototipos(protitype chain)
+const avo = {attr1: 'A' }
+const pai = {__proto__: avo, attr2: 'B' }
+const filho ={__proto__: pai, attr3: 'C' }
+console.log(filho.attr0, filho.attr1, filho.attr2, filho.attr3);
+
+
 const carro = {
   velAtual: 0,
-  velMaz: 200,
+  velMax: 200,
   acelerarMais(delta) {
     if (this.velAtual + delta <= this.velMax) {
       this.velAtual += delta;
@@ -12,3 +19,26 @@ const carro = {
     return `${this.velAtual}km/h de ${this.velMax}km/h`;
   },
 };
+
+const ferrari = {
+    modelo: 'F40',
+    velMax: 324
+}
+
+const volvo = {
+  modelo: 'V40',
+  status() {
+    return `${this.modelo} ${super.status}`
+  }
+}
+
+Object.setPrototypeOf(ferrari, carro)
+Object.setPrototypeOf(volvo, carro)
+
+console.log(ferrari);
+console.log(volvo);
+
+volvo.acelerarMais(100)
+console.log(volvo.status());
+
+
