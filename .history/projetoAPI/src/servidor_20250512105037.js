832071f612f9e5a41aa8655bdd367981e51286f1
Rubenslourenco <alvesrubens926@gmail.com>
2025-05-12 11:16:37 -0300

    feat: iniciando api no node

---
+++ .history/projetoAPI/src/servidor_20250512105037.js
@@ -0,0 +1,15 @@
+const porta = 3003;
+
+const express = require("express");
+const app = express();
+
+app.get("/produtos", (req, res, next) => {
+  res.send({
+    nome: "Notebook",
+    preco: 1234.56,
+  });
+});
+
+app.listen(porta, () => {
+  console.log(`Servidor executando na porta ${porta}.`);
+});
